cbf0c20355429f87856d2cc5146df38bd07f6868
papalouis <papa.louis59@gmail.com>
2018-09-24 21:35:15 +0200

    function del compte

--- src/Banque.cpp
+++ src/Banque.cpp
@@ -27,3 +27,13 @@ void Banque::ajouteCompte(string cpt, float soldeCpt, float decouvMax)
 {
     comptes[nbc] = Compte(cpt, soldeCpt, decouvMax);
 }
+
+void Banque::supprimeCompte(string nomCompte){
+    for (int i = 0; i < nbc; i++){
+      if (nomCompte == comptes[i]->getNomCompte()){
+        for (int j = i; j < nbc; j++){
+          compte[j] = compte[j+1];
+        }
+      }
+    }
+}
