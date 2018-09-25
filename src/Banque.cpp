7a47fa9eac042f861730a4085881eb16492f4a1c
matkirsh <32906629+matkirsh@users.noreply.github.com>
2018-09-25 22:53:08 +0200

    update matt
    Il faut décrémenter nbc dans les fonctions supprimeCompte

--- src/Banque.cpp
+++ src/Banque.cpp
@@ -14,3 +14,36 @@ void Banque::ajouteCompte(string cpt, float soldeCpt, float decouvMax)
 {
         comptes[nbc] = Compte(cpt, soldeCpt, decouvMax);
 }
+
+void Banque::supprimeCompte(string cpt)
+{
+    for (int i(0);i<nbc;i++)
+    {
+        if (comptes[i]== Compte(cpt))
+        {
+            for(int j(i+1);j<nbc;j++)
+            {
+                comptes[j]=comptes[j+1];
+            }
+        }
+    }
+}
+
+void Banque::supprimeCompte(int numCpt)
+{
+    for (int i(0);i<nbc;i++)
+    {
+        if (comptes[i]==Compte(numCpt))
+        {
+            for(int j(i+1);j<nbc;j++)
+            {
+                comptes[j]=comptes[j+1];
+            }
+        }
+    }
+}
+
+void redimensionneTableauComptes(int nouvelleTaille)
+{
+    
+}
